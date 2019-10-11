b555b968c5df0fed7d141098c8e7153b6aeca9d2
Matthew Gall <git@matthewgall.com>
2019-10-11 22:31:17 +0100

    Added Telegram support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Router = require('./router')
+const router = require('./router')
 const rrtypes = [
     'A',
     'AAAA',
@@ -26,9 +26,19 @@ function isInArray(value, array) {
   return array.indexOf(value) > -1;
 }
 
+async function sendTelegram(token, chat_id, message) {
+    let req = await fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}`)
+
+    if (req.status == 200) {
+        return true
+    }
+    return false
+}
+
 async function getDNS(domain, rtype) {
     let records = []
-    req = await fetch(`https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=${domain}&type=${rtype}`, {
+    let req = await fetch(
+        `https://cloudflare-dns.com/dns-query?ct=application/dns-json&name=${domain}&type=${rtype}`, {
         headers: {
             'Accept': 'application/dns-json'
         }
@@ -47,11 +57,11 @@ async function getDNS(domain, rtype) {
 async function handleTwilio(req) {
     let formData = await req.formData()
 
-    let messageFrom = formData.get('MessageSid')
-    let message_from = formData.get('From')
-    let message_body = formData.get('Body')
+    let messageId = formData.get('MessageSid')
+    let messageFrom = formData.get('From')
+    let messageBody = formData.get('Body')
 
-    let data = message_body.trim().split(' ')
+    let data = messageBody.trim().split(' ')
     let domain = ''
     let rtype = 'AAAA'
 
@@ -92,12 +102,84 @@ async function handleTwilio(req) {
     })
 }
 
-function handleTelegram(req) {
-    return false    
+async function handleTelegram(req) {
+    let secretsTelegram = await secrets.get('telegram')
+    secretsTelegram = JSON.parse(secretsTelegram)
+
+    let params = new URL(req.url).searchParams
+    let botName = params.get('bot').toLowerCase()
+
+    if (botName == null) {
+        return new Response(`Unable to determine which bot is being used`, {
+            status: 404,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+
+    let botToken = secretsTelegram[botName]
+    let formData = await req.json()
+
+    let messageId = formData['message']['chat']['id']
+    let messageBody = formData['message']['text']
+
+    let data = messageBody.trim().split(' ')
+    let domain = ''
+    let rtype = 'AAAA'
+
+    let output = ''
+    let ips = ''
+
+    if (data.length > 1) {
+        rtype = data[0].toUpperCase()
+        domain = data[1].toLowerCase()
+    } 
+    else {
+        domain = data[0].toLowerCase()
+    }
+
+    if (isInArray(domain, ['/start', '/stop', '/help'])) {
+        output = `Welcome to 1.1.1.1, a partnership between Cloudflare and APNIC
+
+To get started, simply send the record type (default: AAAA), followed by a space and the hostname you'd like to lookup and I'll answer as quick as I can!`
+    }
+    else {
+        if (isInArray(rtype, rrtypes) == false) {
+            output = `${rtype} is not supported at this time. Please try another record type (e.g AAAA ${domain})`
+        }
+        else {
+            let d = await getDNS(domain, rtype)
+
+            if (d.length > 0) {
+                ips = d.join('\r\n')
+            }
+            else {
+                ips = `We couldn't find any ${rtype} records for ${domain}`
+            }
+
+            output = `${ips}
+
+=======
+1.1.1.1 is a partnership between Cloudflare and APNIC`
+        }
+    }
+
+    let t = await sendTelegram(botToken, messageId, output)
+    return new Response(output, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+}
+
+async function handleEmail(req) {
+    let secrets_email = await secrets.get('email')
+    return false
 }
 
 async function handleRequest(request) {
-    const r = new Router()
+    const r = new router()
 
     // /incoming/*
     r.post('/incoming/telegram', req => handleTelegram(req)) // telegram
